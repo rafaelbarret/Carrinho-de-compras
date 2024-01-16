04ef54f2e0633b00fad7b569d485a4b9e1ae909d
rafaelbarret <rafaelferreiraproductions@gmail.com>
2024-01-15 22:56:58 -0300

    finalização do projeto

--- src/components/CartButton/CartButton.jsx
+++ src/components/CartButton/CartButton.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 import './CartButton.css';
+import AppContext from '../../context/AppContext';
 
 function CartButton() {
+
+  const { cartItems, isCartVisible, setisCartVisible} = useContext(AppContext);
+
   return ( 
-    <button type="button" className="cart__button">
+    <button 
+      type="button" 
+      className="cart__button"
+      onClick={() => setisCartVisible(!isCartVisible)}
+    >
       <AiOutlineShoppingCart />
-      <span className="cart-status">1</span>
+      
+      { cartItems.length > 0 && <span className="cart-status">{cartItems.length}</span>}
     </button>
   );
 }
--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -5,13 +5,23 @@ import AppContext from './AppContext';
 function Provider({ children }) {
 
   const [products, setProducts] = useState ([]);
+  const [cartItems, setCartItems] = useState ([]);
   const [loading, setLoading] = useState(true);
+  const [isCartVisible, setisCartVisible] = useState(false);
+
 
   const value = {
     products,
     setProducts,
+
     loading, 
     setLoading,
+
+    cartItems, 
+    setCartItems,
+
+    isCartVisible, 
+    setisCartVisible,
   };
 
 
